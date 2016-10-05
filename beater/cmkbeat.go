d7fe81ddb16d4f89898ddd885a02b68da2d3b817
Jeremy Weader <jeremyweader@allpropa.com>
2016-10-05 01:09:44 -0400

    fields limit

--- beater/cmkbeat.go
+++ beater/cmkbeat.go
@@ -107,10 +107,10 @@ func (bt *Cmkbeat) lsQuery(lshost string, beatname string) error {
 	if len(filter) > 0 {
 		for _, f := range filter {
 			if strings.HasPrefix(f, "And") {
-				and := strconv.Atoi(strings.TrimPrefix(f, "And: "))
+				and, _ := strconv.Atoi(strings.TrimPrefix(f, "And: "))
 				q.And(and)
 			} else if strings.HasPrefix(f, "Or") {
-				or := strconv.Atoi(strings.TrimPrefix(f, "Or: "))
+				or, _ := strconv.Atoi(strings.TrimPrefix(f, "Or: "))
 				q.Or(or)
 			} else {
 				q.Filter(f)
